3f302957dae64a1e584d17483274d0e7d84a4d2d
Abhinav Maheshwari <abhinavm@gmail.com>
2016-12-13 02:19:49 +0530

    Fixed arity of handler in router schema

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var internals = {};
 var routerSchema = Joi.object().keys({
   path: Joi.string().required(),
   method: Joi.string().required(),
-  handler: Joi.func().arity(2).required()
+  handler: Joi.func().required()
 });
 
 var eventSchema = Joi.object().keys({
